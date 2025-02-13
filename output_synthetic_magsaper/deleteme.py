96f0eebaa382ef421e120e3fce091bda502a45b7
bap37 <bap37@duke.edu>
2025-02-13 10:27:18 -0600

    Various updates to filters

--- output_synthetic_magsaper/deleteme.py
+++ output_synthetic_magsaper/deleteme.py
@@ -2,19 +2,24 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+dfN = pd.read_csv("/project2/rkessler/SURVEYS/PS1MD/USERS/dscolnic/Excalibur_dillon/output_synthetic_magsaper/synth_PS1_shift_0.000.txt.gz", sep=" ")
 
-dfT = pd.read_csv("TEST-synth_CFA3S_shift_0.000.txt", sep=r"\s+")
+#dfN = pd.read_csv("synth_PS1_shift_0.000.txt", sep=" ")
+dfT = pd.read_csv("synth_PS1_shift_0.000.txt", sep=" ")
 
-dfN = pd.read_csv("../output_synthetic_fragilistic/synth_CFA3S_shift_0.000.txt", sep=r"\s+")
-#dfN = pd.read_csv("synth_CFA3S_shift_0.000.txt", sep=r"\s+")
+plt.figure()
+plt.scatter(dfT['PS1-g'], dfT['PS1-g'] + dfN['PS1s_RS14_PS1_tonry-g'], label='g')
+plt.scatter(dfT['PS1-g'], dfT['PS1-r'] + dfN['PS1s_RS14_PS1_tonry-r'], label='r')
+plt.scatter(dfT['PS1-g'], dfT['PS1-i'] + dfN['PS1s_RS14_PS1_tonry-i'], label='i')
+plt.scatter(dfT['PS1-g'], dfT['PS1-z'] + dfN['PS1s_RS14_PS1_tonry-z'], label='z')
+plt.xlim([0,10])
+plt.ylim([-0.025,0.025])
+plt.legend()
 
-dfM = pd.merge(dfT, dfN, on=["standard"])
+plt.savefig("bla.pdf")
 
-print(dfM)
 
-yax = "CFA4P1-r"
-
-#plt.figure()
-for yax in ['CFA3S-B', 'CFA3S-V', 'CFA3S-R', 'CFA3S-I']:
-    print(np.mean(dfM[yax+'_y'] - dfM[yax+'_x']))
-
+print(np.mean(dfT['PS1-g'] + dfN['PS1s_RS14_PS1_tonry-g']))
+print(np.mean(dfT['PS1-r'] + dfN['PS1s_RS14_PS1_tonry-r']))
+print(np.mean(dfT['PS1-i'] + dfN['PS1s_RS14_PS1_tonry-i']))
+print(np.mean(dfT['PS1-z'] + dfN['PS1s_RS14_PS1_tonry-z']))
